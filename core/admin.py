31d8d2979ef50803213f076070812c19339adb13
piyushvcodify <piyushvcodify@gmail.com>
2022-08-25 17:57:07 +0530

    addingfiles

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
+from curses.ascii import EM
 from django.contrib import admin
+from .models import Employee
 
-# Register your models here.
+
+admin.site.register(Employee)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    employee_Id = models.IntegerField()
+    employee_first_name = models.CharField(max_length=150)
+    employee_last_name = models.CharField(max_length=150)
+    job_name  = models.CharField(max_length=150)
+    date_of_joinning  = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return str(self.employee_id)+" : "+self.employee_name
+
+    class Meta:
+        db_table = "tbl_employees"
+        verbose_name = "Employee"
+        verbose_name_plural = "Employee's"
