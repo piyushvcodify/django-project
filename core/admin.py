8dce50be2e18a6f1841720449607b7095bbd46e1
piyushvcodify <piyushvcodify@gmail.com>
2022-08-25 18:31:18 +0530

    addfile

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from curses.ascii import EM
 from django.contrib import admin
-from .models import Employee
+from .models import *
 
 
 admin.site.register(Employee)
+admin.site.register(Department)
+admin.site.register(Role)
--- core/models.py
+++ core/models.py
@@ -1,18 +1,52 @@
 from django.db import models
 
 
+class Team(models.Model):
+    pass
+class Project(models.Model):
+    pass
+
+class Role(models.Model):
+    role_name = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.role_name
+
+    class Meta:
+        db_table = "tbl_role"
+        verbose_name = "Role"
+        verbose_name_plural = "Role"
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=150)
+    department_description = models.TextField(blank=True,null=True)
+    role = models.ManyToManyField(Role)
+    
+    def __str__(self):
+        return self.department_name
+
+    # def save(self,*args, **kwargs):
+    #     self.department_name = self.department_name.title()
+    #     super(Department, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = "tbl_department"
+        verbose_name = "Department"
+        verbose_name_plural = "Department"
+
 class Employee(models.Model):
     employee_Id = models.IntegerField()
     employee_first_name = models.CharField(max_length=150)
     employee_last_name = models.CharField(max_length=150)
     job_name  = models.CharField(max_length=150)
     date_of_joinning  = models.DateField()
+    department_name = models.ForeignKey(Department,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
-        return str(self.employee_id)+" : "+self.employee_name
+        return str(self.employee_Id)+" : "+self.employee_first_name +" "+ self.employee_last_name
 
     class Meta:
         db_table = "tbl_employees"
